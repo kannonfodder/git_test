6a8c51b80ff06c2548778b7abcc80b42947d0a35
kannonfodder <kannonfodder@kannonfoundry.dev>
2021-11-18 20:45:08 +0000

    Add some more text

--- super-mega-app/pages/counter/index.tsx
+++ super-mega-app/pages/counter/index.tsx
@@ -5,6 +5,9 @@ const Counter: NextPage = () =>{
     return (
         <div>
             THIS IS JUST SOME <b>RAW</b> HTML
+
+            HERE IS SOME MORE TEXT!!!!!
+            <i>This is in italics</i>
         </div>
     )
 }
