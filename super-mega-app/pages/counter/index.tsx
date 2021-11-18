207f2f1396e590a30a6f387f0d6057f0975bdf75
kannonfodder <kannonfodder@kannonfoundry.dev>
2021-11-18 20:09:15 +0000

    add new counter page

---
+++ super-mega-app/pages/counter/index.tsx
@@ -0,0 +1,12 @@
+import { NextPage } from "next";
+
+
+const Counter: NextPage = () =>{
+    return (
+        <div>
+            THIS IS JUST SOME <b>RAW</b> HTML
+        </div>
+    )
+}
+
+export default Counter;
